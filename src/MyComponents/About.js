63d24e237e553340de77bbb97e4915bda07df09c
Sarvesh <sarveshgond0000@gmail.com>
2022-12-09 10:54:16 +0530

    react_webapp

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h3>This is all about TO-DO</h3> 
+            <hr />
+            <p>ToDo List App is a kind of app that generally used to maintain our day-to-day tasks or list everything that we have to do, with the most important tasks at the top of the list, and the least important tasks at the bottom. It is helpful in planning our daily schedules. </p>
+            <hr />
+            <h3>This is all about Theprogremmo</h3> 
+            <p>TheProgremmo is a platform initiated by a group of Newbie Engineers for helping others to learn coding & development, to get best suit for the industry so that no one remain unemployed.
+            </p>
+            
+        </div>
+    )
+}
